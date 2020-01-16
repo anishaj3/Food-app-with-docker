9321d27c4b3ec477e013cdd999b0e8e21634bc04
anisha <anisha.j2@cognizant.com>
2020-01-16 22:23:32 +0530

    Updated server.js

--- NutritionistUI/server.js
+++ NutritionistUI/server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-app.use(express.static(__dirname + '/dist'))
+app.use(express.static(__dirname + '/dist/NutritionistUI/'))
 	app.get('/', function(req, res) {
 		res.status(200).send('OK')
 	})
